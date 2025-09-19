5724276165ad2b2f3ca2a4881de1dc316f16486c
Phil Sinatra <code@philsinatra.com>
2025-09-19 16:05:52 -0400

    Fix syntax highlighting bug

--- init.lua
+++ init.lua
@@ -1,2 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_create_autocmd({"BufReadPost"}, {
+  callback = function()
+    vim.defer_fn(function()
+      vim.cmd("filetype detect")
+      vim.cmd("syntax enable")
+      pcall(vim.treesitter.start) -- Force Tree-sitter to attach
+    end, 100) -- Delay 100ms to let Neovim settle
+  end,
+})
